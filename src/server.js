801b98622951b966d419588b82fd483643f8a797
Ates Goral <ates.g@myplanet.com>
2016-10-01 12:06:24 -0400

    Added static file server

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.use('/vendor', express.static(__dirname + '/../node_modules'));
+app.use('/', express.static(__dirname + '/public'));
+
+const port = parseInt(process.env.PORT || '3003', 10);
+
+app.listen(port, () => {
+  console.log('Listening on port ' + port);
+});
