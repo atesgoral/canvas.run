c0767d1ea8f2f73dc40310d0a818821f739de588
Ates Goral <ates.g@myplanet.com>
2016-10-01 22:38:01 -0400

    Allow id in path

--- src/server.js
+++ src/server.js
@@ -9,12 +9,18 @@ mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/canvas-run');
 mongoose.connection.on('open', () => {
   console.log('Connected to DB');
 });
+
 mongoose.connection.on('error', (err) => {
   console.error('DB connection error', err);
 });
 
-app.use('/vendor', express.static(__dirname + '/../node_modules'));
 app.use('/', express.static(__dirname + '/public'));
+
+app.use('/vendor', express.static(__dirname + '/../node_modules'));
+
+app.get('*', (request, response) => {
+  response.sendFile(__dirname + '/public/index.html');
+});
 
 const port = parseInt(process.env.PORT || '5000', 10);
 
