f9263cbf00ef934c6f20809cf0e7b6cde21e6eaa
Ates Goral <ates.g@myplanet.com>
2017-01-10 16:08:20 -0500

    Reordered login buttons

--- client/src/components/SignInPopup.vue
+++ client/src/components/SignInPopup.vue
@@ -2,10 +2,10 @@
   <popup title="Sign in" v-bind:popup="popup" class="sign-in-popup">
     <h3>Sign in using:</h3>
     <div class="_auth-methods">
-      <button type="button" class="-facebook" title="Facebook" v-on:click="auth('facebook')"><span>Facebook</span></button>
-      <button type="button" class="-twitter" title="Twitter" v-on:click="auth('twitter')"><span>Twitter</span></button>
       <button type="button" class="-github" title="GitHub" v-on:click="auth('github')"><span>GitHub</span></button>
+      <button type="button" class="-twitter" title="Twitter" v-on:click="auth('twitter')"><span>Twitter</span></button>
       <button type="button" class="-google" title="Google" v-on:click="auth('google')"><span>Google</span></button>
+      <button type="button" class="-facebook" title="Facebook" v-on:click="auth('facebook')"><span>Facebook</span></button>
     </div>
     <p class="_actions">
       <button type="button" class="_action" v-on:click="popup.close">Cancel</button>
