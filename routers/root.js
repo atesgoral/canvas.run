9ef9cf78009acaa2d38fb13ee2f699763d02bc5b
Ates Goral <ates.g@myplanet.com>
2017-01-31 11:09:06 -0500

    Use path-to-regexp for identical path matching

--- routers/root.js
+++ routers/root.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const pathToRegexp = require('path-to-regexp');
 
 const paths = require('../client/src/paths.json');
 
 const router = express.Router();
 
-function sendClientIndex(res) {
+function sendPage(req, res) {
   res.sendFile('index.html', {
     root: __dirname + '/../client/dist'
   });
@@ -15,6 +16,8 @@ router.get('/', (req, res) => {
   // res.send('<h1>Homepage</h1>');
 });
 
-Object.keys(paths).forEach((key) => router.get(paths[key], (req, res) => sendClientIndex(res)));
+Object
+  .keys(paths)
+  .forEach((key) => router.get(pathToRegexp(paths[key], []), sendPage));
 
 module.exports = router;
